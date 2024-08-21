e8bdc8d16934c367cdbf8828a5a8bf469e80f824
bkgcet <root@ip-172-31-45-149.ap-south-1.compute.internal>
2024-08-21 09:21:40 +0000

    1st commit for chef-server

---
+++ chef.io.sh
@@ -0,0 +1,165 @@
+Instance ID
+
+i-02cc836c9efc179f2 (Node1)
+Public IPv4 address
+
+3.110.45.204 | open address 
+Private IPv4 addresses
+
+172.31.1.6
+IPv6 address
+–
+Instance state
+Running
+Public IPv4 DNS
+
+ec2-3-110-45-204.ap-south-1.compute.amazonaws.com
+|
+open address 
+Hostname type
+IP name: ip-172-31-1-6.ap-south-1.compute.internal
+Private IP DNS name (IPv4 only)
+
+ip-172-31-1-6.ap-south-1.compute.internal
+Answer private resource DNS name
+IPv4 (A)
+Instance type
+t2.micro
+Elastic IP addresses
+–
+Auto-assigned IP address
+
+3.110.45.204 [Public IP]
+VPC ID
+
+vpc-00a5f6b267958850b 
+AWS Compute Optimizer finding
+Opt-in to AWS Compute Optimizer for recommendations.
+|
+Learn more 
+IAM Role
+–
+Subnet ID
+
+subnet-07ffc20fbb728e161 
+Auto Scaling Group name
+–
+IMDSv2
+Required
+Instance ARN
+
+arn:aws:ec2:ap-south-1:058264325573:instance/i-02cc836c9efc179f2
+
+Details
+
+Status and alarms
+
+Monitoring
+
+Security
+
+Networking
+
+Storage
+
+Tags
+Instance details
+Info
+Platform
+
+Red Hat (Inferred)
+AMI ID
+
+ami-022ce6f32988af5fa
+Monitoring
+disabled
+Platform details
+
+Red Hat Enterprise Linux
+AMI name
+
+RHEL-9.4.0_HVM-20240605-x86_64-82-Hourly2-GP3
+Termination protection
+Disabled
+Stop protection
+Disabled
+Launch time
+
+Wed Aug 21 2024 14:12:48 GMT+0530 (India Standard Time) (13 minutes)
+AMI location
+
+amazon/RHEL-9.4.0_HVM-20240605-x86_64-82-Hourly2-GP3
+Instance auto-recovery
+Default
+Lifecycle
+normal
+Stop-hibernate behavior
+Disabled
+AMI Launch index
+0
+Key pair assigned at launch
+
+node-key
+State transition reason
+–
+Credit specification
+standard
+Kernel ID
+–
+State transition message
+–
+Usage operation
+
+RunInstances:0010
+RAM disk ID
+–
+Owner
+
+058264325573
+Enclaves Support
+–
+Boot mode
+
+uefi-preferred
+Current instance boot mode
+
+legacy-bios
+Allow tags in instance metadata
+Disabled
+Use RBN as guest OS hostname
+
+Disabled
+Answer RBN DNS hostname IPv4
+
+Enabled
+Host and placement group
+Info
+Host ID
+–
+Affinity
+–
+Placement group
+–
+Host resource group name
+–
+Tenancy
+
+default
+Placement group ID
+–
+Virtualization type
+
+hvm
+Reservation
+
+r-0c59ad90080ebcbeb
+Partition number
+–
+Number of vCPUs
+1
+Capacity reservation
+Info
+Capacity Reservation ID
+–
+Capacity Reservation setting
+open
